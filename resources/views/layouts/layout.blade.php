29d34cc5c9cf29a0450046a4ff6e6a502b1c5c0d
Robinson Legaspi <robinson.legaspi@yahoo.com>
2016-07-18 01:37:17 +0800

    done animation

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<meta charset="utf-8">
-    <meta name="_token" content="{!! csrf_token() !!}"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta id="_token" value="{{ csrf_token() }}">
